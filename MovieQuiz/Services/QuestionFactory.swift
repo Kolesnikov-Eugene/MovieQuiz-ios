5e64e86b1c8b1a74ec5d528e9feda9c188f85b8a
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2023-03-30 21:56:51 +0300

    final version with error alert

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -44,7 +44,8 @@ class QuestionFactory: QuestionFactoryProtocol {
             do {
                 imageData = try Data(contentsOf: movie.resizedImageURL)
             } catch {
-                DispatchQueue.main.async {
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
                     self.delegate?.didFailToLoadData(with: error)
                 }
             }
